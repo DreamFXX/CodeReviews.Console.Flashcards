c513437558b056effc77b20a18bc0e26a045a0eb
DreamFXX <filipek.husak@gmail.com>
2024-12-09 15:29:43 +0100

    Create SqlConn

--- DreamFXX.Flashcards/Program.cs
+++ DreamFXX.Flashcards/Program.cs
@@ -1,6 +1,14 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Data.SqlClient;
 
-string? connectionString = new ConfigurationManager().GetConnectionString("ConnectionString");
+string? connectionString = @"Data Source=localhost;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
-
+using SqlConnection connection = new SqlConnection(connectionString);
+connection.Open();
+SqlCommand command = connection.CreateCommand();
+command.CommandText = @"CREATE TABLE IF NOT EXISTS Flashcards_Table (
+                                                   Id INT IDENTITY(1,1),
+                                                   PRIMARY KEY (Id),
+                                                   Name TEXT NOT NULL)";
+command.ExecuteNonQuery();
+connection.Close();
